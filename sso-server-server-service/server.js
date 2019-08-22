c6c59b4d0e14d39cfef035b85888212bcaa3ac69
somulu <gugulothusomulu06@gmail.com>
2019-08-22 11:10:46 +0530

    logout button is added

--- sso-server-server-service/server.js
+++ sso-server-server-service/server.js
@@ -4,55 +4,13 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
 const cors = require("cors");
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 // const apps = require("./app");
 const users = require("./routes/api/users");
 
 const app = express();
 app.use(cors());
-/*    CORS Middleware         */
-// const whitelist = ["http://sso-server-client.cloud.somulu.com:3020/"];
-// const corsOptions = {
-//   origin: function(origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   }
-// };
-// app.use(cors(corsOptions));
-// app.use(function(req, res, next) {
-//   var oneof = false;
-//   if (req.headers.origin) {
-//     res.header("Access-Control-Allow-Origin", req.headers.origin);
-//     oneof = true;
-//   }
-//   if (req.headers["access-control-request-method"]) {
-//     res.header(
-//       "Access-Control-Allow-Methods",
-//       req.headers["access-control-request-method"]
-//     );
-//     oneof = true;
-//   }
-//   if (req.headers["access-control-request-headers"]) {
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       req.headers["access-control-request-headers"]
-//     );
-//     oneof = true;
-//   }
-//   if (oneof) {
-//     res.header("Access-Control-Max-Age", 60 * 60 * 24 * 365);
-//   }
-
-//   // intercept OPTIONS method
-//   if (oneof && req.method == "OPTIONS") {
-//     res.send(200);
-//   } else {
-//     next();
-//   }
-// });
 
 app.all("*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -69,11 +27,24 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
-mongoose
-  .connect(db, { useNewUrlParser: true }) // Let us remove that nasty deprecation warrning :)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
+// mongoose
+//   .connect(db, { useNewUrlParser: true }) // Let us remove that nasty deprecation warrning :)
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch(err => console.log(err));
+mongoose.connect(db, { useNewUrlParser: true });
+/* Session is added */
+app.use(
+  session({
+    secret: "abcdefgh",
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 },
+    store: new MongoStore({
+      url: db,
+      collection: "session"
+    })
+  })
+);
 // Passport middleware
 app.use(passport.initialize());
 
